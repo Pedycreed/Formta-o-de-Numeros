1534b5971991c998b4e4e1312f551f7101bd9d05
Jhonatan _Salgueiro <jhonatansalgueiro2002@gmail.com>
2025-04-09 15:26:49 -0300

    app do codigo feito pelo electron

---
+++ main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 450,
+    height: 650,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true
+    }
+  })
+
+  win.loadFile(path.join(__dirname, 'formatar-2.0.html'))
+  win.setMenuBarVisibility(false) // Remove a barra de menu
+}
+
+app.whenReady().then(createWindow)
